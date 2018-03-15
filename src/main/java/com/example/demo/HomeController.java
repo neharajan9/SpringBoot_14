fb2731b3c43383eda63876cf9b92b98f34c32e44
GBTC408002UR <GBTC408002UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-03-15 12:02:09 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,49 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Controller
+public class HomeController {
+    @Autowired
+    DirectorRepository directorRepository;
+
+    @RequestMapping("/")
+    public String index(Model model){
+        //First create director
+        Director director = new Director();
+        director.setName("Stephen Bullock");
+        director.setGenre("Sci Fi");
+
+        //create Movie
+        Movie movie = new Movie();
+        movie.setTitle("star Movie");
+        movie.setYear(2017);
+        movie.setDescription("About stars....");
+
+        //Add movie to an empty list
+        Set<Movie> movies = new HashSet<Movie>();
+        movies.add(movie);
+
+        movie= new Movie();
+        movie.setTitle("DeathStar Ewoks");
+        movie.setYear(2011);
+        movie.setDescription("About Ewoks on the DeathStar...");
+        movies.add(movie);
+
+        //add list of movies to the director's movie list
+        director.setMovies(movies);
+
+        //save the director to the database
+        directorRepository.save(director);
+
+        //Grad all the directors from the database and send them to the template
+        model.addAttribute("directors", directorRepository.findAll());
+        return "index";
+    }
+}
